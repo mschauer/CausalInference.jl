7049454b011e7bc5b326a3eb5a7097cc2bfd8e3f
mschauer <moritzschauer@web.de>
2017-08-25 10:15:41 +0200

    save adjacency matrix

--- test/perf.jl
+++ test/perf.jl
@@ -8,15 +8,15 @@ if !isfile(joinpath("data","NCI-60.csv"))
     cd("data") do
         run(`wget http://nugget.unisa.edu.au/ParallelPC/data/real/NCI-60.csv`)
     end
-end 
-let 
-    data = readcsv("data/NCI-60.csv")
-    d = size(data,2)
-    X = data[:,1:d]'
-    d, n = size(X)
-    C = Symmetric(cor(X, 2))
-    p = 0.01
-    @time h, s = skeleton(d, gausscitest, (C,n), quantile(Normal(), 1-p/2)) 
-    println("inferred edges ", ne(h))
+end  
 
-end
+data = readcsv("data/NCI-60.csv")
+d = size(data,2)
+X = data[:,1:d]'
+d, n = size(X)
+C = Symmetric(cor(X, 2))
+p = 0.01
+@time h, s = skeleton(d, gausscitest, (C,n), quantile(Normal(), 1-p/2)) 
+println("inferred edges ", ne(h))
+writecsv("data/adjac.csv", adjacency_matrix(h))
+
