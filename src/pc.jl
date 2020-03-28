47949377934bf5d5e25b6af500a508e0fb2108bf
Martin Keller <schnirz@gmail.com>
2020-03-28 11:06:13 +0000

    changed argument types for plot_pc_graph

--- src/pc.jl
+++ src/pc.jl
@@ -249,16 +249,18 @@ end
 
 plot the output of the PC algorithm.
 """
-function plot_pc_graph(g, df)
+function plot_pc_graph(g, node_labels::Array=[])
     plot_g = DiGraph(nv(g))
 
+    if length(node_labels) != nv(g) 
+        node_labels = map(string, 1:nv(g))
+    end
+        
     node_style = "draw, rounded corners, fill=blue!10"
     options = "scale=2"
     
     styles_dict = Dict()
-    
-    columns = map(string, Tables.columnnames(df))
-    
+        
     for e in edges(g)
        if e.src < e.dst
             add_edge!(plot_g, e.src, e.dst)
@@ -270,6 +272,6 @@ function plot_pc_graph(g, df)
         end
     end
     
-    TikzGraphs.plot(plot_g, columns, edge_styles=styles_dict,
+    TikzGraphs.plot(plot_g, node_labels, edge_styles=styles_dict,
                     node_style=node_style, options=options)
 end
