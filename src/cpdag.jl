e8fdc0bcd991a122c6f6ae4a0ec64f8608c2e348
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2023-06-25 12:39:51 +0200

    typo

--- src/cpdag.jl
+++ src/cpdag.jl
@@ -4,7 +4,7 @@ import Base: iterate, length
     alt_cpdag(g::DiGraph)
 
 Computes CPDAG from a DAG using a simpler adaption of Chickering's conversion algorithm without ordering all edges
-(equivalent to the PC algorithm with `d`-seperation as independence test, see `pc_oracle`.)
+(equivalent to the PC algorithm with `d`-separation as independence test, see `pc_oracle`.)
 
 Reference: M. Chickering: Learning equivalence classes of Bayesian
 network structures. Journal of Machine Learning Research 2 (2002).
