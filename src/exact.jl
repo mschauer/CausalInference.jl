815385677f8b9ef973fab5f1cc21098ceae5dbf4
Marcel Wienöbst <marcel.wienoebst@gmx.de>
2024-01-06 17:51:59 +0100

    give full reference to paper

--- src/exact.jl
+++ src/exact.jl
@@ -77,8 +77,10 @@ end
 """ 
     exactscorebased(X; method=:gaussian_bic, penalty=0.5, parallel=false, verbose=false)
 
-Compute a CPDAG for the given observed data `X` (variables in columns) using the exact algorithm proposed by Silander and Myllymäki in https://arxiv.org/pdf/1206.6875.pdf for optimizing the BIC score (or any decomposable score).  
+Compute a CPDAG for the given observed data `X` (variables in columns) using the exact algorithm proposed by Silander and Myllymäki (2006) for optimizing the BIC score (or any decomposable score).  
 The complexity is n*2^n and the algorithm should scale up to ~20-25 variables, afterwards memory becomes a problem.
+
+* Silander, T., & Myllymäki, P. (2006). A simple approach for finding the globally optimal Bayesian network structure. In Uncertainty in Artificial Intelligence (pp. 445-452). 
 """
 function exactscorebased(X::AbstractMatrix; method=:gaussian_bic, penalty=0.5, parallel=false, verbose=false)
     (_, n) = size(X)
@@ -96,6 +98,7 @@ function exactscorebased(X::AbstractMatrix; method=:gaussian_bic, penalty=0.5, p
         throw(ArgumentError("method=$method"))
     end
 end
+exactscorebased(X; method=:gaussian_bic, penalty=0.5, parallel=false, verbose=false) = exactscorebased(Tables.matrix(X); method, penalty, parallel, verbose)
 
 function exactscorebased(n, local_score; parallel=false, verbose=false)
     # Step 1: calculate score for all n*2^(n-1) different (variable, variable set) pairs (variable and parents)
