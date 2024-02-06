57bda87983199f8215ffcc99d3ddf630c90087c0
Moritz Schauer <moritzschauer@web.de>
2024-02-06 21:00:44 +0100

    Update test/dag_sampler.jl

--- test/dag_sampler.jl
+++ test/dag_sampler.jl
@@ -2,7 +2,7 @@ using Random, CausalInference, Statistics, Test, Graphs
 @testset "Dag-Zig-Zag" begin
     Random.seed!(1)
 
-    iterations = 500_000
+    iterations = 5_000
     n = 3
     gs = dagzigzag(n; iterations);
     graphs, graph_pairs, hs, τs, ws, ts, scores = CausalInference.unzipgs(gs);
