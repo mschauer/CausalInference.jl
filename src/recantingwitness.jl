17dfe92535eae964722beb725a4f15255f722691
Moritz Schauer <moritzschauer@web.de>
2024-02-18 13:22:40 +0100

    Update src/recantingwitness.jl

--- src/recantingwitness.jl
+++ src/recantingwitness.jl
@@ -4,7 +4,7 @@ export has_recanting_witness
     has_recanting_witness(g::AbstractGraph, u, v,  blocked_edges::AbstractGraph) -> Bool
 
 In a causal DAG with edges `g`, determine whether path-specific causal effect from vertex `u` to `v`
-with edges in `blocked_edges` blocked can be can be computed uniquely from the data available to the investigator,
+with edges in `blocked_edges` blocked can be can be computed uniquely from the data available to the investigator (assuming complete observations),
 which is the case if there is no "recanting witness".
 Essentially this means that `blocked_edges` could equivalently be replaced by a blocking only
 outgoing edges of `u`. 
