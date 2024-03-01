37ca08238c34841fdf347ab425b04a2f4d9fa123
mschauer <moritzschauer@web.de>
2024-03-01 22:48:21 +0100

    More extensive tests

--- test/multisampler.jl
+++ test/multisampler.jl
@@ -2,7 +2,7 @@ using Random, CausalInference, Statistics, Test, Graphs, LinearAlgebra
 @testset "MultiSampler" begin
     Random.seed!(1)
 
-    N = 2000 # number of data points
+    N = 500 # number of data points
 
     # define simple linear model with added noise
     x = randn(N)
@@ -24,7 +24,55 @@ using Random, CausalInference, Statistics, Test, Graphs, LinearAlgebra
     # maximum aposteriori estimate
     MAP = [1=>2, 1=>3, 2=>1, 2=>4, 3=>1, 3=>4, 4=>5]
     @test bestgraph == digraph(MAP, n)
-    @test abs(-(extrema(getfield.(last.(samplers), :τ))...)) < 0.1
     cm = sort(countmap(vpairs.(getfield.(last.(samplers), :g))), byvalue=true, rev=true)
     @test first(cm).first == MAP
 end #testset
+
+if false # if you have time 
+@testset "MultiSampler" begin
+    Random.seed!(1)
+
+    N = 200 # number of data points
+
+    # define simple linear model with added noise
+    x = randn(N)
+    v = x + randn(N)*0.25
+    w = x + randn(N)*0.25
+    z = v + w + randn(N)*0.25
+    s = z + randn(N)*0.25
+    
+    df = (x=x, v=v, w=w, z=z, s=s)
+    iterations = 400
+    penalty = 2.0 # increase to get more edges in truth
+    n = length(df) # vertices
+    Random.seed!(101)
+    C = cor(CausalInference.Tables.matrix(df))
+    score = GaussianScore(C, N, penalty)
+    M = 200
+    bestgraph, samplers = multisampler(n; M, σ=10.0, score, iterations)
+    coldness = CausalInference.expcoldness(minimum(getfield.(last.(samplers), :τ)))
+
+    gs = causalzigzag(n; score, κ=n-1, coldness, iterations)
+    graphs, graph_pairs, hs, τs, ws, ts, scores = CausalInference.unzipgs(gs)
+    posterior = sort(keyedreduce(+, graph_pairs, ws); byvalue=true, rev=true)
+
+
+    # maximum aposteriori estimate
+    MAP = [1=>2, 1=>3, 2=>1, 2=>4, 3=>1, 3=>4, 4=>5]
+    @test bestgraph == digraph(MAP, n)
+    cm = sort((proportionmap(vpairs.(getfield.(last.(samplers), :g)))), byvalue=true, rev=true)
+    @test first(cm).first == MAP
+    logΠ = map(g->score_dag(pdag2dag!(digraph(g, n)), score), collect(keys(cm)))
+    Π = normalize(exp.(coldness*(logΠ .- maximum(logΠ))), 1)
+    Πhat = normalize(collect(values(cm)), 1)
+    display([Π Πhat])
+    s = 0.0
+    for (i, k) in enumerate(keys(cm))
+        s += posterior[k]
+        @show cm[k] Π[i] 
+    end
+    @show s
+    @test s > 0.98
+    @test norm(collect(values(cm)) - Π) < 0.02
+end #testset
+end
