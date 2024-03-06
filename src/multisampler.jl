6256f22146c7e5f8cee072b7a30aac1004931241
mschauer <moritzschauer@web.de>
2024-03-06 09:03:48 +0100

    Factoring

--- src/multisampler.jl
+++ src/multisampler.jl
@@ -4,11 +4,14 @@ struct Sample
     dir::Int8
     total::Int32 
     scoreval::Float64
+    alive::Bool
 end
+Sample(g, nextτ, dir, total, scoreval) = Sample(g, nextτ, dir, total, scoreval, true) 
 
 struct Action
+    i::Int
     τ::Float64 
-    apply::Function
+    apply!::Function
     args::Tuple{Vararg{Any}}
 end
 
@@ -27,31 +30,44 @@ end
 function applyup(samplers, i, nextτ, x, y, T, Δscoreval)
     prevsample = samplers[i]
     g = next_CPDAG(prevsample.g, :up, x, y, T)
-    return Sample(g, nextτ, prevsample.dir, prevsample.total+1, prevsample.scoreval + Δscoreval)
+    return samplers[i] = Sample(g, nextτ, prevsample.dir, prevsample.total+1, prevsample.scoreval + Δscoreval)
 end
 
 function applydown(samplers, i, nextτ, x, y, H, Δscoreval)
     prevsample = samplers[i]
     g = next_CPDAG(prevsample.g, :down, x, y, H)
-    return Sample(g, nextτ, prevsample.dir, prevsample.total-1, prevsample.scoreval + Δscoreval) 
+    return samplers[i] = Sample(g, nextτ, prevsample.dir, prevsample.total-1, prevsample.scoreval + Δscoreval) 
 end
 
 function applyflip(samplers, i, nextτ)
     prevsample = samplers[i]
-    return Sample(prevsample.g, nextτ, -1*prevsample.dir, prevsample.total, prevsample.scoreval)
+    return samplers[i] = Sample(prevsample.g, nextτ, -1*prevsample.dir, prevsample.total, prevsample.scoreval)
 end
 
-function applycopy(samplers, _, nextτ, j)
+function applycopy(samplers, i, nextτ, j)
     copysample = samplers[j]
-    return Sample(copysample.g, nextτ, copysample.dir, copysample.total, copysample.scoreval)
+    return samplers[i] = Sample(copysample.g, nextτ, copysample.dir, copysample.total, copysample.scoreval)
+end
+
+function applykill(samplers, i, nextτ)
+    prevsample = samplers[i]
+    return samplers[i] = Sample(prevsample.g, nextτ, prevsample.dir, prevsample.total, prevsample.scoreval, false)
+end
+
+function applynothing(samplers, i, nextτ)
+    @assert false
+    sample = samplers[i]
+    return samplers[i] = Sample(sample.g, nextτ, sample.dir, sample.total, sample.scoreval, sample.alive)
 end
 
 # for starters without turn move
 const baseline_ = [0.0]
 
-function sampleaction(samplers, i, M, balance, prior, score, σ, ρ, κ, coldness, Dcoldness, threshold) 
+function sampleaction(samplers, i, M, balance, prior, score, σ, ρ, κ, coldness, Dcoldness, threshold, keep) 
     # preprocess 
     prevsample = samplers[i]
+    prevsample.alive || return Action(i, Inf, applynothing, ())
+ 
     sup, sdown, Δscorevalup, Δscorevaldown, argsup, argsdown = count_moves_new(prevsample.g, κ, balance, prior, score, coldness(prevsample.τ), prevsample.total)
     global baseline_
     # propose moves
@@ -71,33 +87,37 @@ function sampleaction(samplers, i, M, balance, prior, score, σ, ρ, κ, coldnes
     @assert Δτmin >= 0 
     if :dir == A
         if prevsample.dir == 1
-            return Action(prevsample.τ + Δτdir, applyup, (argsup..., Δscorevalup))
+            return Action(i, prevsample.τ + Δτdir, applyup, (argsup..., Δscorevalup))
         else 
-            return Action(prevsample.τ + Δτdir, applydown, (argsdown..., Δscorevaldown))
+            return Action(i, prevsample.τ + Δτdir, applydown, (argsdown..., Δscorevaldown))
         end
     end
 
     if :updown == A
         λup = sup
         if rand() < λup/λupdown
-            return Action(prevsample.τ + Δτupdown, applyup, (argsup..., Δscorevalup))
+            return Action(i, prevsample.τ + Δτupdown, applyup, (argsup..., Δscorevalup))
         else 
-            return Action(prevsample.τ + Δτupdown, applydown, (argsdown..., Δscorevaldown))
+            return Action(i, prevsample.τ + Δτupdown, applydown, (argsdown..., Δscorevaldown))
         end
     end
     
     if :flip == A
-        return Action(prevsample.τ + Δτflip, applyflip, ())
+        return Action(i, prevsample.τ + Δτflip, applyflip, ())
     end
 
     if :term == A
-        return Action(prevsample.τ + Δτterm, applycopy, (rand(1:M),)) 
+        if rand() < keep
+            return Action(i, prevsample.τ + Δτterm, applycopy, (rand(1:M),)) 
+        else
+            return Action(i, prevsample.τ + Δτterm, applykill, ()) 
+        end    
     end
 
     @assert false
 end
 # remark: chose κ = n-1 as default
-function multisampler(n, G = (DiGraph(n), 0); M = 10, balance = metropolis_balance, prior = (_,_) -> 1.0, score=UniformScore(), σ = 0.0, ρ = 1.0, κ = n - 1, baseline = 0.0, iterations = min(3*n^2, 50000), schedule=(expcoldness, Dexpcoldness), threshold=Inf) #, verbose = false, save = true)
+function multisampler(n, G = (DiGraph(n), 0); M = 10, balance = metropolis_balance, prior = (_,_) -> 1.0, score=UniformScore(), σ = 0.0, ρ = 1.0, κ = n - 1, baseline = 0.0, iterations = min(3*n^2, 50000), schedule=(expcoldness, Dexpcoldness), threshold=Inf, keep=1.0) #, verbose = false, save = true)
     if κ >= n 
         κ = n - 1
         @warn "Truncate κ to $κ"
@@ -108,12 +128,11 @@ function multisampler(n, G = (DiGraph(n), 0); M = 10, balance = metropolis_balan
     baseline_[] = baseline
     # init M samplers
     samplers = [Sample(G[1], 0.0, 1, G[2], 0.0) for _ = 1:M] # pass correct initial score?!
-    nextaction = Vector{Action}(undef, M)
-    queue = PriorityQueue{Int32, Float64}()
+    queue = PriorityQueue{Action, Float64}()
     
     for i = 1:M 
-        nextaction[i] = sampleaction(samplers, i, M, balance, prior, score, σ, ρ, κ, coldness, Dcoldness, threshold)
-        enqueue!(queue, i, nextaction[i].τ)
+        action = sampleaction(samplers, i, M, balance, prior, score, σ, ρ, κ, coldness, Dcoldness, threshold, keep)
+        enqueue!(queue, action, action.τ)
     end
 
     # todo: multiply iterations by M to keep passed iteration number indep of M?
@@ -123,21 +142,24 @@ function multisampler(n, G = (DiGraph(n), 0); M = 10, balance = metropolis_balan
     bestgraph = DiGraph(n)
     bestscore = 0.0 # fix if correct initial score is given above
     count = 0
+    particles = M
     @showprogress for iter in 1:iterations 
-        i = dequeue!(queue)
-        count += nextaction[i].apply == applycopy
-        nextsample = nextaction[i].apply(samplers, i, nextaction[i].τ, nextaction[i].args...)
-        if nextsample.scoreval > bestscore 
+        action = dequeue!(queue)
+        count += action.apply! == applycopy
+        particles -= action.apply! == applykill
+        particles == 0 && break
+
+        nextsample = action.apply!(samplers, action.i, action.τ, action.args...)
+        if nextsample.alive && nextsample.scoreval > bestscore 
             bestgraph = nextsample.g
             bestscore = nextsample.scoreval
         end
-        samplers[i] = nextsample
-        nextaction[i] = sampleaction(samplers, i, M, balance, prior, score, σ, ρ, κ, coldness, Dcoldness, threshold)
-        enqueue!(queue, i, nextaction[i].τ)
+        action = sampleaction(samplers, action.i, M, balance, prior, score, σ, ρ, κ, coldness, Dcoldness, threshold, keep)
+        enqueue!(queue, action, action.τ)
         # todo: applyflip shouldn't increase counter
     end
     killratio = count/iterations
     @show killratio
 
-    return bestgraph, samplers
+    return bestgraph, [sample for sample in samplers if sample.alive]
 end
